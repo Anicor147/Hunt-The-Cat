c7dc05657f7cb96da7d27c1b1baa2617ff908dfa
Anicor147 <antho.nicu.cor147@gmail.com>
2022-11-07 13:54:33 -0500

    Screen Shake
    ScreenShake

--- Hunt the Cat/Assets/Scripts/Shooting.cs
+++ Hunt the Cat/Assets/Scripts/Shooting.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Shooting : MonoBehaviour
 {
 
+    
     public Transform FirePoint;
     public GameObject bulletPrefab;
 
     public float bulletForce = 20f;
+    public float firespeed = 0.5f;
 
+    public float ShakeIntensity = 0f;
+    public float ShakeTime = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+
+        
+        if(Input.GetButtonDown("Fire1") )
         {
             Shoot();
+              Shake.Instance.ShakeCamera(ShakeIntensity,ShakeTime);
         }    
+
     }
 
 
@@ -29,6 +38,7 @@ void Shoot()
 
     body.AddForce(FirePoint.up * bulletForce , ForceMode2D.Impulse);
 
+    
 
 }
 
